b2b48daba67db14ff7545730b3932c3bbcd80596
iZeaoGamer <jameshayward14@icloud.com>
2018-01-17 10:31:38 +0000

    Add friends plugin part 1

---
+++ src/PluginCore/Plugins/FriendsPE/cancelrequest.php
@@ -0,0 +1,22 @@
+<?php
+namespace PluginsCore\Plugins\FriendsPE;
+
+use pocketmine\scheduler\PluginTask;
+use pocketmine\plugin\Plugin;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
+
+class cancelrequest extends PluginTask{
+	private $target;
+	private $requestp;
+	public function __construct(Plugin $owner, Player $target, Player $requestp){
+		parent::__construct($owner);
+		$this->target = $target;
+		$this->requestp = $requestp;
+	}
+	
+	public function onRun(int $currentTick){
+		echo "ran cancelrequest!";
+         $this->getOwner()->removeRequest($this->target,$this->requestp, 0);
+	}
+}
